5c09ffbdea0fd5847aea755a576eb51aa899729b
vboxuser <vicferolv@alu.edu.gva.es>
2025-10-16 00:01:26 +0200

    carpeta raiz

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+</head>
+<body>
+
+    <h1>Q3</h1>
+
+    <?php
+        $mensaje = "Texto para el ejercicio de la Q3";
+        echo $mensaje;
+    ?>
+
+</body>
+</html>
